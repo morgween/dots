c756ec0b8a39bef75b657495962211ec65ceabd6
Maxim <maks9609@gmail.com>
2023-11-29 14:42:03 +0200

    add files

---
+++ sketchybar/items/media.sh
@@ -0,0 +1,26 @@
+media=(
+  icon=
+  icon.label.drawing=off 
+  icon.color=0xff7cfc00
+  icon.font="Hack Nerd Font:Regular:16.0" 
+  icon.background.drawing=on
+  icon.background.image=media.artwork
+  icon.background.image.corner_radius=9
+  script="$PLUGIN_DIR/media.sh"
+  label.max_chars=25
+  scroll_texts=on
+  updates=on
+)
+
+status_bracket=(
+  background.color=$BACKGROUND_1
+  background.border_color=$BACKGROUND_2
+)
+
+
+sketchybar --add item media center \
+           --set media "${media[@]}" \
+           --subscribe media media_change
+           
+sketchybar --add bracket music media \
+           --set music "${status_bracket[@]}"
